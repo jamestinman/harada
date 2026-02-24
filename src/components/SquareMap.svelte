32f08fd269025d2b463401a7f28aa11019703c80
James Leeds <james.leeds@gmail.com>
2026-02-24 14:23:04 +0000

    Update SquareMap.svelte

--- src/components/SquareMap.svelte
+++ src/components/SquareMap.svelte
@@ -68,7 +68,7 @@
 
 <button
 	type="button"
-class="inline-flex flex-col gap-0.5 rounded-lg p-1 border-2 transition-all hover:bg-slate-800/50 active:scale-95 cursor-pointer {store.saveStatus == 'dirty' ? 'border-amber-500' : store.saveStatus == 'saving' ? 'border-red-500' : 'border-transparent'} "
+class="inline-flex flex-col gap-0.5 rounded-lg p-1 border-4 transition-all hover:bg-slate-800/50 active:scale-95 cursor-pointer {store.saveStatus == 'dirty' ? 'border-amber-500' : store.saveStatus == 'saving' ? 'border-red-500' : 'border-transparent'} "
 	onclick={() => {
     goto("/");
 	}}
